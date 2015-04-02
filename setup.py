9593be491c5dc4d39725edb12e10ae54f4a6163f
Krzysztof Wrobel <Krzysztof.Wrobel@unicosoftware.com>
2015-04-02 12:06:03 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='nlp_polish',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=['nlp_polish'],
+      install_requires=[
+          'networkx',
+      ],
+      zip_safe=False)
